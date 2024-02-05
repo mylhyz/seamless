07744c8346c2781ec933ecbc95f1173ffa8ce950
mylhyz <mylhyz@gmail.com>
2024-02-05 14:56:24 +0800

    修复Android端不支持的异常问题

--- xml/src/main/java/org/seamless/xml/SAXParser.java
+++ xml/src/main/java/org/seamless/xml/SAXParser.java
@@ -71,7 +71,7 @@ public class SAXParser {
             factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
             factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
 //            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-            factory.setXIncludeAware(false);
+//            factory.setXIncludeAware(false);
 
             factory.setNamespaceAware(true);
 
